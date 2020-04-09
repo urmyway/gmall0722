b5b8ecba90a752fe85d4086c7e7f75fe56c7bfa0
zhanghuafeng_vendor <zhanghuafeng_vendor>
2020-04-09 14:10:27 +0800

    mr.zhang 上传了一段代码

--- demo/src/main/java/com/atguigu/gmall/demo/controller/HelloController.java
+++ demo/src/main/java/com/atguigu/gmall/demo/controller/HelloController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @PostMapping
+    public String local(){
+        return "hi";
+    }
 }
